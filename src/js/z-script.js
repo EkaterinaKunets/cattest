adbdcd4ff2f3f191b3c53d9a7719283865ed0280
CKutsenko <kate040491@mail.ru>
2016-11-25 17:34:20 +0300

    +promo-slider

--- src/js/z-script.js
+++ src/js/z-script.js
@@ -1,4 +1,4 @@
-window.$ = window.jQuery =  require('jquery');
+// window.$ = window.jQuery =  require('jquery');
 // window.slick =              require('./vendor/bower/slick');
 
 jQuery(document).ready(function($){
@@ -12,63 +12,31 @@ jQuery(document).ready(function($){
 
   /*Бросить на .news__inner одинаковую высоту блока */
 
-  // $('.nav__subhamburger-1').click(function(e){
-  //   e.preventDefault();
-  //   $('.nav__sub-menu--1').toggle();
-  //   $('.nav__subhamburger-1').toggleClass('subhamburger-active');
-  // });
+  $('.nav__subhamburger-1').click(function(e){
+    e.preventDefault();
+    $('.nav__sub-menu--1').toggle();
+    $('.nav__subhamburger-1').toggleClass('subhamburger-active');
+  });
 
-  //   $('.nav__subhamburger-2').click(function(e){
-  //   e.preventDefault();
-  //   $('.nav__sub-menu--2').toggle();
-  //   $('.nav__subhamburger-2').toggleClass('subhamburger-active');
-  // });
+    $('.nav__subhamburger-2').click(function(e){
+    e.preventDefault();
+    $('.nav__sub-menu--2').toggle();
+    $('.nav__subhamburger-2').toggleClass('subhamburger-active');
+  });
 
-  // /* галерея Gratitude */
-  // $('.gratitude__gallery').slick({
-  //   infinite: true,
-  //   arrows: true,
-  //   dots: false,
-  //   slidesToShow: 2,
-  //   slidesToScroll: 1,
-  //   centerPadding: '0',
-  //   variableWidth: true
-  // });
-
-
-  // /* галерея "с нами уже работают" */
-  // if ($(window).width() <= 480) {
-  //   $('.clients__gallery').slick({
-  //     infinite: true,
-  //     arrows: true,
-  //     dots: false,
-  //     slidesToShow: 1,
-  //     slidesToScroll: 1,
-  //     centerMode: true,
-  //     centerPadding: '0'
-  //   });
-  // } else if ($(window).width() <= 768) {
-  //   $('.clients__gallery').slick({
-  //     infinite: true,
-  //     arrows: true,
-  //     dots: false,
-  //     slidesToShow: 4,
-  //     slidesToScroll: 1,
-  //     centerMode: true,
-  //     centerPadding: '0'
-  //   });
-  // } else {
-  //   $('.clients__gallery').slick({
-  //     infinite: true,
-  //     arrows: true,
-  //     dots: false,
-  //     slidesToShow: 7,
-  //     slidesToScroll: 1,
-  //     centerMode: true,
-  //     centerPadding: '0'
-  //   });
-  // }
-
+  /* галерея Promo Slider */
+  $('.promo-slider').slick({
+    infinite: true,
+    arrows: true,
+    dots: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    speed: 900,
+    fade: true,
+    cssEase: 'linear',
+    autoplay: true,
+    autoplaySpeed: 4000
+  });
 
   // /* кнопка "наверх" */
   // $('.side-fixed-menu__link--up').click(function(e) {
